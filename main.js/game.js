b42fec0af4abb3e9a572b11800235f8a8c6aefe4
Amir2210 <107459404+Amir2210@users.noreply.github.com>
2023-11-08 19:31:50 +0200

    Add files via upload
    expandShown function aded

--- main.js/game.js
+++ main.js/game.js
@@ -8,11 +8,19 @@ const gLevel = {
 }
 
 var gBoard
-
+var gGame
 function initGame() {
+  gGame = {
+    isOn: false,
+    shownCount: 0,
+    markedCount: 0,
+    secsPassed: 0,
+    isFirstClick: true
+  }
   gBoard = buildBoard()
-  setMinesNegsCount(gBoard)
+  // setMinesNegsCount(gBoard)
   renderBoard(gBoard)
+  setMinesNegsCount(gBoard)
   // gBoard[findEmptyCell()][findEmptyCell()].isMine = true
   // console.log(gBoard[3][3])
   // console.log("gBoard[0][0] :", gBoard[0][0].i)
@@ -28,7 +36,7 @@ function buildBoard() {
         minesAroundCount: 0,
         isShown: false,
         isMine: false,
-        isMarked: true,
+        isMarked: false,
         i,
         j
       }
@@ -38,10 +46,8 @@ function buildBoard() {
   // board[getRandomInt(0, 4)][getRandomInt(0, 4)].isMine = true
   // board[getRandomInt(0, 4)][getRandomInt(0, 4)].isMine = true
   // board[getRandomInt(0, 4)][getRandomInt(0, 4)].isMine = true
-  board[0][0].isMine = true
-  board[2][2].isMine = true
-  board[3][3].isMine = true
 
+  putMines(board)
   return board
 }
 
@@ -54,7 +60,7 @@ function renderBoard(board) {
       var cellClass = getClassName({ i, j })
       // currCell.isMine ? (cellClass += " floor ") : (cellClass += " floor")
       cellClass += " floor "
-      strHTML += `\t<td class="cell ${cellClass}" onclick="cellClick(this, ${i},${j})">`
+      strHTML += `\t<td data-i="${i}" data-j="${j}" class="cell ${cellClass}" onclick="cellClick(this, ${i},${j})">`
       // if (currCell.isMine) {
       //   strHTML += MINE
       // }
@@ -70,7 +76,7 @@ function renderBoard(board) {
 
 function cellClick(elCell, rowIdx, colIdx) {
   console.log(elCell)
-  // console.log(gBoard[rowIdx][colIdx])
+  var board = gBoard
   if (!gBoard[rowIdx][colIdx].isMine) {
     elCell.innerText = gBoard[rowIdx][colIdx].minesAroundCount
   }
@@ -78,6 +84,7 @@ function cellClick(elCell, rowIdx, colIdx) {
     elCell.innerText = MINE
     elCell.classList.add("mine")
   }
+  expandShown(board, elCell, rowIdx, colIdx)
   elCell.classList.add("revealed")
 }
 
@@ -97,6 +104,39 @@ function setMinesNegsCount(board) {
           var currCell = board[k][l]
           if (currCell.isMine) currSelectedCell.minesAroundCount++
         }
+      }
+    }
+  }
+}
+
+function putMines(board) {
+  board[0][0].isMine = true
+  board[2][2].isMine = true
+  board[3][3].isMine = true
+}
+
+function expandShown(board, elCell, i, j) {
+  console.log(board)
+  console.log(elCell)
+  console.log(i)
+  console.log(j)
+  var rowIdx = i
+  var colIdx = j
+  var board = board
+
+  for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
+    if (i < 0 || i >= board.length) continue
+
+    for (var j = colIdx - 1; j <= colIdx + 1; j++) {
+      if (j < 0 || j >= board[0].length) continue
+      if (i === rowIdx && j === colIdx) continue
+      var currCell = board[i][j]
+      console.log(currCell, i, j)
+      if (!currCell.isMine) {
+        const curr = document.querySelector(`[data-i="${i}"][data-j="${j}"]`)
+        // console.log(curr)
+        curr.classList.add("revealed")
+        curr.innerText = currCell.minesAroundCount
       }
     }
   }
