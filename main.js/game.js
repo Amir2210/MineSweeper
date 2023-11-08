7249e59eaf6510fbf0994ab4c576325b1baea944
Amir2210 <107459404+Amir2210@users.noreply.github.com>
2023-11-08 15:35:40 +0200

    Add files via upload
    first commit

---
+++ main.js/game.js
@@ -0,0 +1,116 @@
+"use strict"
+
+const MINE = "💣"
+const FLOOR = " "
+const gLevel = {
+  SIZE: 4,
+  MINES: 2
+}
+
+var gBoard
+
+function initGame() {
+  gBoard = buildBoard()
+  renderBoard(gBoard)
+
+  // console.log(gBoard[0][1])
+  setMinesNegsCount(gBoard)
+
+  console.log(gBoard[0][1])
+  // console.log("gBoard[0][0] :", gBoard[0][0].i)
+  // console.log(setMinesNegsCount(gBoard, gBoard[0][1]))
+  // console.log("gBoard[0][1].minesAroundCount :", gBoard[0][1].minesAroundCount)
+}
+
+function buildBoard() {
+  const board = createMat(gLevel.SIZE, gLevel.SIZE)
+  for (var i = 0; i < board.length; i++) {
+    for (var j = 0; j < board[0].length; j++) {
+      var cell = {
+        minesAroundCount: 0,
+        isShown: false,
+        isMine: false,
+        isMarked: true,
+        i,
+        j
+      }
+      board[i][j] = cell
+    }
+  }
+  board[0][0].isMine = true
+  board[3][3].isMine = true
+  return board
+}
+
+function renderBoard(board) {
+  var strHTML = ""
+  for (var i = 0; i < board.length; i++) {
+    strHTML += "<tr>\n"
+    for (var j = 0; j < board[0].length; j++) {
+      const currCell = board[i][j]
+      var cellClass = getClassName({ i, j })
+      currCell.isMine ? (cellClass += " mine") : (cellClass += " floor")
+      strHTML += `\t<td class="cell ${cellClass}" onclick="cellClick(this, ${i},${j})">`
+      if (currCell.isMine) {
+        strHTML += MINE
+      }
+      strHTML += FLOOR
+
+      strHTML += "</td>\n"
+    }
+    strHTML += "</tr>\n"
+  }
+  // console.table(strHTML)
+  const elBoard = document.querySelector(".board")
+  elBoard.innerHTML = strHTML
+}
+
+function cellClick(elCell, rowIdx, colIdx) {
+  console.log(elCell)
+  // console.log(rowIdx)
+  // console.log(colIdx)
+}
+
+function setMinesNegsCount(board) {
+  for (var i = 0; i < board.length; i++) {
+    for (var j = 0; j < board[0].length; j++) {
+      var currSelectedCell = board[i][j]
+      var rowIdx = board[currSelectedCell.i][currSelectedCell.j].i
+      var colIdx = board[currSelectedCell.i][currSelectedCell.j].j
+      console.log(currSelectedCell, rowIdx, colIdx)
+      var counter = 0
+      console.log("counter:", counter)
+      if (currSelectedCell.isMine) continue
+      for (var k = rowIdx - 1; k <= rowIdx + 1; k++) {
+        if (k < 0 || k >= board.length) continue
+
+        for (var l = colIdx - 1; l <= colIdx + 1; l++) {
+          if (l < 0 || l >= board[0].length) continue
+          if (k === rowIdx && l === colIdx) continue
+          var currCell = board[k][l]
+          console.log(currCell)
+          if (currCell.isMine) counter++
+        }
+      }
+      counter++
+      console.log("counter:", counter)
+      currSelectedCell.minesAroundCount = counter
+    }
+  }
+}
+
+// function neighboursCounter(row, col, bor) {
+//   var counter = 0
+//   for (var i = row - 1; i <= row + 1; i++) {
+//     if (i < 0 || i >= bor.length) continue
+
+//     for (var j = col - 1; j <= col + 1; j++) {
+//       if (j < 0 || j >= bor[0].length) continue
+//       if (i === row && j === col) continue
+//       var currCell = bor[i][j]
+//       // console.log(currCell)
+//       if (currCell.isMine) counter++
+//     }
+//   }
+//   counter++
+// }
---
+++ main.js/utils.js
@@ -0,0 +1,62 @@
+"use strict"
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min) + min) // The maximum is exclusive and the minimum is inclusive
+}
+
+function findEmptyCell() {
+  const emptyCells = []
+  for (var i = 0; i < gBoard.length; i++) {
+    for (var j = 0; j < gBoard[i].length; j++) {
+      const cell = gBoard[i][j]
+      if (!cell.gameElement && cell.type === EMPTY) {
+        emptyCells.push({ i, j })
+      }
+    }
+  }
+
+  if (!emptyCells.length) return null
+
+  const randIdx = getRandomInt(0, emptyCells.length)
+  return emptyCells[randIdx]
+}
+
+// function neighboursCount() {
+//   var counter = 0
+//   for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
+//     if (i < 0 || i >= board.length) continue
+
+//     for (var j = colIdx - 1; j <= colIdx + 1; j++) {
+//       if (j < 0 || j >= board[0].length) continue
+//       if (i === rowIdx && j === colIdx) continue
+//       var currCell = board[i][j]
+//       if (currCell === BALL) ballCounter++
+//     }
+//   }
+//   ballCounter++
+// }
+
+function createMat(ROWS, COLS) {
+  const mat = []
+
+  for (var i = 0; i < ROWS; i++) {
+    const row = []
+    for (var j = 0; j < COLS; j++) {
+      row.push("")
+    }
+    mat.push(row)
+  }
+  return mat
+}
+
+function renderCell(location, value) {
+  const elCell = document.querySelector(`.cell-${location.i}-${location.j}`)
+  elCell.innerHTML = value
+}
+
+function getClassName(position) {
+  const cellClass = `cell-${position.i}-${position.j}`
+  return cellClass
+}
